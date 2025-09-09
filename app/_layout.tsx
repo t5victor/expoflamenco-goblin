e7c39a2315bf85559b6e18b5aa12c831be2daa31
unknown <victor@expoflamenco.com>
2025-09-09 23:28:09 +0200

    More Improvements towards RC
    Following the latest update, I am still refining the software for the Release Candidate version. This time, I’ve added OpenGraph tags, a new web title, and a favicon. I’ll be testing and experimenting to see how it looks.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import Head from 'expo-router/head';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -19,6 +20,14 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Head>
+        <title>Expoflamenco - Admin Panel</title>
+        <meta name="description" content="Goblin is a groundbreaking new Expo capability that lets managers stealthily unlock and seize high-value data intelligence." />
+        <meta property="og:title" content="Expoflamenco - Admin Panel" />
+        <meta property="og:description" content="Goblin is a groundbreaking new Expo capability that lets managers stealthily unlock and seize high-value data intelligence." />
+        <meta property="og:image" content="/images/EF512.png" />
+        <link rel="icon" href="/images/EF512.png" />
+      </Head>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
